99626f30708789b67e2b177d4b6482b23000771f
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-05-21 17:22:47 +0300

    Added reviews section

--- app/js/main.js
+++ app/js/main.js
@@ -4,4 +4,48 @@ $(function () {
         $(".header__menu-list").toggleClass("header__menu-list--active");
         $(".header__logo").toggleClass("header__logo--hide");
     });
+
+    // Reviews slider
+
+    $('.reviews__items').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 7000,
+        speed: 2000,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [{
+                breakpoint: 1921,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 508,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false,
+                    autoplaySpeed: 3000,
+                }
+            }
+        ]
+    });
 })
