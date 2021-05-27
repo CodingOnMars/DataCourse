aeca534d145a42d46a689c06d0912f08502ed4de
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-05-27 16:35:03 +0300

    Added jobs slider

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,58 @@ $(function () {
         $(".header__logo").toggleClass("header__logo--hide");
     });
 
+    // Jobs slider
+
+    $('.jobs__items').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 7000,
+        speed: 2000,
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        responsive: [{
+                breakpoint: 1441,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 5,
+                }
+            },
+            {
+                breakpoint: 1102,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4
+                }
+            },
+            {
+                breakpoint: 882,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 682,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 508,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false,
+                    autoplaySpeed: 3000,
+                }
+            }
+        ]
+    });
+
+
     // Reviews slider
 
     $('.reviews__items').slick({
@@ -13,7 +65,7 @@ $(function () {
         infinite: true,
         autoplay: true,
         autoplaySpeed: 7000,
-        speed: 2000,
+        speed: 6000,
         slidesToShow: 4,
         slidesToScroll: 4,
         responsive: [{
